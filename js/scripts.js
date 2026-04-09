4d6217ef307b3c3116f003a03deb8272203132ba
stanley alexander martinez portillo <stantroom@hotmail.com>
2026-04-08 18:21:41 -0600

    finalizacion de practica

--- js/scripts.js
+++ js/scripts.js
@@ -59,6 +59,49 @@ const width = window.innerWidth;
 
 //alert("su pantalla posee "+width+" pixeles de ancho y "+height+" pixeles de alto");
 console.log("su pantalla posee"+width+"pixeles de ancho y "+height+"pixeles de alto");
+console.log("window.screen.avail.Width :"+window.screen.width+"px","window.screen.height:"+window.screen.height+"px");
 console.log("window.screen.avail.Width :"+window.screen.availWidth+"px","window.screen.availHeight:"+window.screen.availHeight+"px");
-
 console.log("document.body.clientWidth:"+document.body.clientWidth+"px","document.body.clientHeight:"+document.body.clientHeight+"px");
+
+function calcular(psumando,ssumando,salida){
+
+    //sa capturan los valores del html
+    primerSumando = document.getElementById(psumando);
+    segundoSumando = document.getElementById(ssumando);
+
+    //se realiza el calculo
+    let calculo = (parseFloat(primerSumando.value) + parseFloat(segundoSumando.value));
+
+    //se asigna el valor al campo de salida
+    document.getElementById(salida).innerHTML = calculo;
+    
+}
+
+function crearElemento(url = null){
+    if(url !== null){
+        url = url;
+    }else{
+        url = "https://as2.ftcdn.net/jpg/01/09/00/83/220_F_109008395_OQDupHMza1V6CNOzrJwWAKlaktT4IsRW.jpg";
+    }
+
+    //se crea un nuevo elemnto html
+    let objetoimagen = document.createElement("img");
+
+    //se asigna propiedades con valores especificos
+    objetoimagen.id = "imagencreada";
+    objetoimagen.src = url;
+    objetoimagen.alt = "Icono sobre control de videojuegos";
+    objetoimagen.title = "Control de videojuego";
+    objetoimagen.width = 220;
+    objetoimagen.height = 165;
+
+    //se agrega una escucha al evento click;
+    objetoimagen.addEventListener('click',function(){
+        objetoimagen.src = "https://encrypted-tbn0.gstatic.com/imagenes?q=tbn:ANd9GcTkU5595AC-bbsoPF-Ik0WzUbVj9rnAbs9lkw&s";
+        objetoimagen.width = 225;
+        objetoimagen.height = 225;
+    });
+
+    //se "pega" o "adhiere" al lienzo (<div>)
+    document.getElementById('lienzo').appendChild(objetoimagen);
+}
